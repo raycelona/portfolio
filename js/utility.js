7e09f767c53ca4a4469de9449d300dbbd7dc4dc9
Ray Celona <rayc7780@gmail.com>
2020-09-22 17:18:23 -0400

    convert JS functions to jQuery

--- js/utility.js
+++ js/utility.js
@@ -3,54 +3,31 @@
 
 $(document).ready(function () {
 
-    setTimeout(() => {
-        $(".advert").slideDown();
-    }, 1000)
-    $("#hide").click(function () {
-        $("p").toggle();
+    $(".sliding-link").click(function(e) {
+        e.preventDefault();
+        let aid = $(this).attr("href");
+        $('html, body').animate({ scrollTop: $(aid).offset().top }, 'slow');
     });
 
-    $(".sliding-link").click(function (e) {
-        e.preventDefault();
-        var aid = $(this).attr("href");
-        $('html,body').animate({ scrollTop: $(aid).offset().top }, 'slow');
+    $(window).scroll(function () {
+        let aboutPos = $("#about");
+        let stickyStart = aboutPos.offset().top;
+        if (window.pageYOffset >= stickyStart - 1) {
+            $("header").addClass("stickyNav");
+        } else {
+            $("header").removeClass("stickyNav");
+        }
     });
 
-    $(".toTop").click(function (e) {
-        e.preventDefault();
-        var aid = $(this).attr("href");
-        $('html,body').animate({ scrollTop: $(aid).offset().top }, 'fast');
-    });
-    $(".historyBtn").click(function () {
-        // $(".workhistory").slideToggle("fast");
-        $("#history").css("height", "600px");
-    });
-    $(".techBtn").click(function () {
-        $(".moretech").slideToggle("fast");
+    $(".workItem:first").show();
+    $("#history li:first").addClass("active");
+    $("#history li").click(function () {
+        index = $(this).index();
+        $("#history li").removeClass("active");
+        $(this).addClass("active");
+        $(".workItem").hide();
+        $(".workItem").eq(index).show();
     });
 
 });
 
-
-/**** JS Utilities ****/
-
-function changeDesc(company) {
-    let workItem = document.getElementsByClassName("workItem");
-    for (let i = 0; i < workItem.length; i++) {
-        workItem[i].style.display = "none";
-    }
-    document.getElementById(company).style.display = "block";
-};
-
-(function stickyNav() {
-    window.addEventListener('scroll', () => {
-        let aboutPos = document.getElementById("about");
-        let stickyStart = aboutPos.offsetTop;
-        if(window.pageYOffset >= stickyStart - 1) {
-            let header = document.getElementById("header");
-            header.classList.add("stickyNav")
-        } else {
-            header.classList.remove("stickyNav")
-        }
-    })
-})();
